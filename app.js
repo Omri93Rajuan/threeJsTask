04581234cc397c9b42e50e0059eb0848f7237028
Omri93Rajuan <omri93rajuan@gmail.com>
2024-08-05 10:25:05 +0300

    from blitz

--- app.js
+++ app.js
@@ -44,4 +44,3 @@ window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-});

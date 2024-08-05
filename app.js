7db4f1dfe6cea1a225c9ca209f0d5fb107a3d40a
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2024-08-05 11:37:52 +0300

    work model on OrbitControls

--- app.js
+++ app.js
@@ -44,3 +44,4 @@ window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+});
